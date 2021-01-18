4fefc009012131ac4e7e22baac9da61496e759ef
mai-sha <shamai1213@outlook.com>
2021-01-17 21:44:50 -0500

    useState, custome hooks, update ESLint, bring in petfinder client

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { render } from "react-dom";
-import Pet from "./Pet";
+import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <h1>Adopt Me!</h1>
+      <SearchParams />
     </div>
   );
 };
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
